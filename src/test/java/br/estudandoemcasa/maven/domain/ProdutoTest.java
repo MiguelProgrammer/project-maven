f77bad42170acfd7a9a2e4cca6ce99ef87f6ad2b
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-09-25 01:31:25 -0300

    Teste com Jacoco

--- src/test/java/br/estudandoemcasa/maven/domain/ProdutoTest.java
+++ src/test/java/br/estudandoemcasa/maven/domain/ProdutoTest.java
@@ -1,14 +1,18 @@
 package br.estudandoemcasa.maven.domain;
 
-import static org.junit.Assert.*;
+import java.math.BigDecimal;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+		Produto p = new Produto("Teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", p.getNome());
+		Assert.assertEquals(BigDecimal.TEN, p.getValor());
 	}
 
 }
